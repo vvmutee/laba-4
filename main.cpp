349b12b776ae0b635552198276e6c8791950fa35
Arkhip <108357450+vvmutee@users.noreply.github.com>
2024-10-09 22:02:27 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+//N1
+
+/*int main()
+{
+    int A, B, C;
+    cin >> A >> B >> C;
+    
+    if (A < B && B > C){
+        cout << B+A-C;
+    }
+    if (B % C == 0){
+        cout << B/(C - B);
+    }
+    else{
+        cout << (A * B) - C;
+    }
+    
+}*/
+
+//N2
+
+/*int main()
+{
+    int n;
+    cin >> n;
+    
+    switch(n){
+        case 1:
+        cout << "January";
+        break;
+        
+        case 2:
+        cout << "February";
+        break;
+        
+        case 3:
+        cout << "March";
+        break;
+        
+        case 4:
+        cout << "April";
+        break;
+        
+        case 5:
+        cout << "May";
+        break;
+        
+        case 6:
+        cout << "June";
+        break;
+        
+        case 7:
+        cout << "July";
+        break;
+        
+        case 8:
+        cout << "August";
+        break;
+        
+        case 9:
+        cout << "September";
+        break;
+        
+        case 10:
+        cout << "October";
+        break;
+        
+        case 11:
+        cout << "November";
+        break;
+        
+        case 12:
+        cout << "December";
+        break;
+        
+        default:
+            cout << "Error";
+            break;
+    }
+}*/
+
+//N3
+
+/*int main()
+{
+    int x;
+    cin >> x;
+    if (x == 1) cout << "Positive number";
+    else cout << "Negative number";
+}
+*/
+/*Eсли используем switch, то 
+программа выполняет 2 проверки, используя условие.
+Благодаря if мы делаем одну проверку и отсекаем 
+другой вариант ответа.*/
+
